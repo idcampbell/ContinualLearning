916461a10cd364e3987ff03c4b890fc3b83ad92f
tylergiallanza <tylergiallanza@gmail.com>
2023-04-21 16:44:17 -0400

    Update model architecture and plotting code
    - Allow model to have a varying number of hidden units
    - Allow model to have a varying number of independent layers
    - Plot model predictions and true values side-by-side
    - Compare model rep structure to environment structure

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+from plotly.subplots import make_subplots
 import torch
 
 def set_torch_device(use_gpu=True):
@@ -11,3 +13,13 @@ def set_torch_device(use_gpu=True):
     else:
         device = torch.device('cpu')
     return device
+
+def plot_predictions(train_y, model_predictions, n_contexts):
+    n_rows = max(1,n_contexts//2)
+    fig = make_subplots(rows=n_rows,cols=4,row_heights=[1]*n_rows,column_widths=[1,1,1,1],
+                        subplot_titles=('True','Predicted','True','Predicted'))
+    for context_idx in range(n_contexts):
+        fig.add_trace(px.imshow(train_y[context_idx]).data[0],row=context_idx//2+1,col=1+2*(context_idx%2))
+        fig.add_trace(px.imshow(model_predictions[context_idx]).data[0],row=context_idx//2+1,col=2+2*(context_idx%2))
+    fig.update_layout(height=300*n_rows,width=300*4)
+    fig.show()
